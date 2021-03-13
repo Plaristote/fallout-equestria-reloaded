d5d86ff0b9e741130645b192c19a8fa7d80fcec2
Michael <michael@unetresgrossebite.com>
2021-03-13 04:05:38 +0100

    implemented metabolism task for characters

--- scripts/pnjs/character.mjs
+++ scripts/pnjs/character.mjs
@@ -8,6 +8,21 @@ export class CharacterBehaviour {
     this.textBubbles = [
       { content: "Lorem ipsum dolor sit amet, et toute ces sortes de choses.", duration: 5000, color: "yellow" }
     ];
+    if (!this.model.hasVariable("metabolismEnabled"))
+    {
+      this.model.setVariable("metabolismEnabled", true);
+      this.model.tasks.addTask("updateMetabolism", 86400000, 0);
+    }
+  }
+
+  updateMetabolism() {
+    if (this.model.isAlive()) {
+      const maxHitPoints = this.model.statistics.maxHitPoints;
+      const healingRate  = this.model.statistics.healingRate;
+      const currentHp    = this.model.statistics.hitPoints;
+      
+      this.model.statistics.hitPoints = Math.min(currentHp + healingRate, maxHitPoints);
+    }
   }
 
   getAvailableInteractions() {
