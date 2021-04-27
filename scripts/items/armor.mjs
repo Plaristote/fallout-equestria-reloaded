1dce70206677ba270f386d33a226c6682f8c7bcc
Michael <michael@unetresgrossebite.com>
2021-04-28 00:05:00 +0200

    implements ArmorBehaviour script helper

---
+++ scripts/items/armor.mjs
@@ -0,0 +1,25 @@
+import {ItemBehaviour} from "./item.mjs";
+
+export class ArmorBehaviour extends ItemBehaviour {
+  constructor(model) {
+    super(model);
+    this.triggersCombat = false;
+    this.useModes = ["use"];
+    this.armorClass = 0;
+  }
+  
+  canEquipInSlotType(slotType) {
+    return slotType == "armor";
+  }
+
+  onEquipped(user, on) {
+    if (on)
+      user.statistics.armorClass += this.armorClass;
+    else
+      user.statistics.armorClass -= this.armorClass;
+  }
+}
+
+export function create(model) {
+  return new ArmorBehaviour(model);
+}
