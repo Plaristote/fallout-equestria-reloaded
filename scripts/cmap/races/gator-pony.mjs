75333c212ae73c9bf26b85e90fc885a381186742
Michael <michael@unetresgrossebite.com>
2021-05-17 01:48:53 +0200

    add gator-pony spritesheet and race

---
+++ scripts/cmap/races/gator-pony.mjs
@@ -0,0 +1,25 @@
+export const withFaceColor = true;
+
+export function spriteSheet(model) {
+  return {
+    cloneOf:    "gator-pony",
+    base:       "gator-pony",
+    staticBase: "gator-pony-eyes",
+    color:      model.faceColor
+  };
+}
+
+export function onToggled(statistics, toggled) {
+  var modifier = toggled ? 1 : -1;
+
+  statistics.strength  += (1 * modifier);
+  statistics.charisma  -= (1 * modifier);
+}
+
+export function modifyBaseSkill(characterSheet, name, value) {
+  switch (name) {
+  case "outdoorsman":
+    return value + 25;
+  }
+  return value;
+}
