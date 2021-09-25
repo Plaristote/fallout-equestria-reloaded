249a90ad71f57388e1e6ed5db277b35bace326f5
Michael <michael@unetresgrossebite.com>
2021-09-25 11:17:55 +0200

    * refactore LevelRender.mjs * fix mouse cursor not rendering in level editor

--- game/LevelRender.mjs
+++ game/LevelRender.mjs
@@ -1,11 +1,10 @@
-import {MouseMode, TargetMode} from "./Interaction.mjs";
+import {CursorController} from "./level/Cursor.mjs";
 import {initializeRenderQueue, fillRenderQueue} from "./RenderQueue.mjs";
 
-export class Controller {
+export class Controller extends CursorController {
   constructor(canvas, params) {
+    super(canvas, params);
     this.pathPrefix = params.pathPrefix
-    this.canvas   = canvas;
-    this.context  = canvas.getContext("2d");
     this.tilemap  = params.tilemap;
     this.level    = params.level;
     this.rootPath = params.rootPath;
@@ -21,10 +20,6 @@ export class Controller {
       misc:   this.tilemap.getLayer("misc")
     };
     this.frameCount = 0;
-  }
-
-  get origin() {
-    return this.canvas.origin;
   }
 
   isObjectVisible(object, detectedCharacters) {
@@ -48,10 +43,7 @@ export class Controller {
     this.eachCase(this.renderCoordinates.bind(this));
     this.renderVisualEffects();
     this.renderRoofs();
-    if (this.level.mouseMode === MouseMode.Movement) // if cursor is movement mode
-      this.renderMoveCursor();
-    if (this.level.mouseMode === MouseMode.Target && this.level.targetMode == TargetMode.Zone)
-      this.renderZoneCursor();
+    super.render();
     this.frameCount++;
   }
 
@@ -124,80 +116,6 @@ export class Controller {
       offset.x,   offset.y,   width, height,
       position.x, position.y, width, height
     );
-  }
-
-  renderZoneCursor() {
-    if (this.level.player && this.level.isCharacterTurn(this.level.player)) {
-      const coordinates = this.canvas.hoverTile;
-      const item = this.level.activeItem;
-
-      if (item && coordinates && coordinates.length === 2) {
-        const from = { x: coordinates[0] - item.zoneSize, y: coordinates[1] - item.zoneSize };
-        const to   = { x: coordinates[0] + item.zoneSize, y: coordinates[1] + item.zoneSize };
-
-        for (var x = from.x ; x <= to.x ; ++x) {
-          for (var y = from.y ; y <= to.y ; ++y) {
-            const tile = this.layers.ground.getTile(x, y);
-
-            if (tile)
-              this.renderImage(`../assets/ui/cursors/target-tile.png`, tile.renderPosition, this.tileSize.width, this.tileSize.height);
-          }
-        }
-      }
-    }
-  }
-
-  renderCombatMoveCursor() {
-    if (this.level.player && this.level.isCharacterTurn(this.level.player)) {
-      const coordinates = this.canvas.hoverTile;
-
-      if (coordinates && coordinates.length === 2) {
-        const path = this.level.previewPathTo(coordinates[0], coordinates[1]);
-        const maxLength = Math.min(this.level.player.actionPoints, path.length);
-
-        for (var i = 0 ; i < maxLength ; ++i) {
-          const tile = this.layers.ground.getTile(path[i].x, path[i].y);
-          const lastTile = i + 1 === maxLength;
-          const texture = lastTile ? "move-tile.png" : "move-tile-unavailable.png";
-
-          if (tile) {
-            this.renderImage(`../assets/ui/cursors/${texture}`, tile.renderPosition, this.tileSize.width, this.tileSize.height);
-            if (lastTile) {
-              this.context.font = "bold 20px sans-serif";
-              this.context.fillStyle = "#FFFFFF";
-              this.context.fillText(`${maxLength}`, tile.renderPosition.x + this.tileSize.width / 2 - 10, tile.renderPosition.y + this.tileSize.height / 2);
-              this.context.strokeStyle = "#000000";
-              this.context.strokeText(`${maxLength}`, tile.renderPosition.x + this.tileSize.width / 2 - 10, tile.renderPosition.y + this.tileSize.height / 2);
-            }
-          }
-        }
-        this.canvas.hoverTileHintVisible = !(maxLength !== path.length || path.length === 0);
-        return true;
-      }
-    }
-    return false;
-  }
-
-  renderMoveCursor() {
-    if (!this.renderCombatMoveCursor())
-    {
-      const coordinates = this.canvas.hoverTile;
-
-      if (coordinates && coordinates.length === 2) {
-        const tile = this.layers.ground.getTile(coordinates[0], coordinates[1]);
-
-        if (tile)
-        {
-          const occupied = this.level.grid.isOccupied(coordinates[0], coordinates[1]);
-          const texture  = !occupied ? "move-tile.png" : "move-tile-unavailable.png";
-
-          this.canvas.hoverTileHintVisible = true;
-          this.renderImage(`../assets/ui/cursors/${texture}`, tile.renderPosition, this.tileSize.width, this.tileSize.height);
-          return ;
-        }
-      }
-      this.canvas.hoverTileHintVisible = false;
-    }
   }
 
   renderCoordinates(x, y) {
@@ -356,37 +274,6 @@ export class Controller {
     return false;
   }
 
-  renderSprite(sprite) {
-    const offset      = sprite.spritePosition;
-    const clippedRect = sprite.clippedRect;
-
-    if (this.shouldRender(offset.x, offset.y, clippedRect.width, clippedRect.height)) {
-      this.context.drawImage(
-        this.pathPrefix + sprite.spriteSource,
-        clippedRect.x, clippedRect.y, clippedRect.width, clippedRect.height,
-        offset.x, offset.y, clippedRect.width, clippedRect.height
-      );
-      return true;
-    }
-  }
-
-  renderImage(source, tileOffset, width, height, clippedRect) {
-    const extraHeight = height - this.tileSize.height;
-    const offset  = { x: tileOffset.x, y: tileOffset.y - extraHeight };
-
-    if (clippedRect === undefined)
-      clippedRect = Qt.rect(0, 0, width, height);
-    if (this.shouldRender(offset.x, offset.y, width, height)) {
-      this.context.drawImage(
-        source,
-        clippedRect.x, clippedRect.y, clippedRect.width, clippedRect.height,
-        offset.x, offset.y, width, height
-      );
-      return true;
-    }
-    return false;
-  }
-
   getPointFor(x, y) {
     return Qt.point(
       x * this.tileSize.width  / 2 - y * this.tileSize.width  / 2,
@@ -403,53 +290,5 @@ export class Controller {
       maxX: (this.mapSize.height * this.tileSize.width / 2) + this.canvas.width / 2,
       minY: -rect.height + this.canvas.height / 2
     };
-  }
-
-  shouldRender(x, y, width, height)   {
-    return x + width  >= -this.origin.x && x <= -this.origin.x + this.canvas.width
-        && y + height >= -this.origin.y && y <= -this.origin.y + this.canvas.height;
-  }
-
-  clear() {
-    this.context.clearRect(-this.origin.x, -this.origin.y, this.canvas.width * 10, this.canvas.height * 10);
-  }
-
-  getHoveredCase(posX, posY) {
-    for (var x = 0 ; x < this.mapSize.width; ++x) {
-      for (var y = 0 ; y < this.mapSize.height; ++y) {
-        const pos = this.getPointFor(x, y);
-        if (posX >= pos.x && posX <= pos.x + this.tileSize.width &&
-            posY >= pos.y && posY <= pos.y + this.tileSize.height)
-        {
-          return [x, y];
-        }
-      }
-    }
-    return null;
-  }
-
-  onMouseClick(mouse, mouseX, mouseY) {
-    if (!this.level.combat || this.level.isPlayerTurn) {
-      mouseX -= this.canvas.origin.x;
-      mouseY -= this.canvas.origin.y;
-      if (this.level.mouseMode === MouseMode.Movement || (this.level.mouseMode === MouseMode.Target && this.level.targetMode === TargetMode.Zone))
-        this.onCaseClick(mouseX, mouseY);
-      else if (this.level.mouseMode === MouseMode.Target || this.level.mouseMode === MouseMode.Interaction)
-        this.onObjectClick(mouseX, mouseY);
-    }
-  }
-
-  onCaseClick(mouseX, mouseY) {
-    const coords = this.getHoveredCase(mouseX, mouseY);
-
-    if (coords !== null)
-      this.level.tileClicked(coords[0], coords[1]);
-  }
-
-  onObjectClick(mouseX, mouseY) {
-    const object = this.level.getObjectAt(mouseX, mouseY);
-
-    if (object !== null)
-      this.level.objectClicked(object);
   }
 };
---
+++ game/level/Cursor.mjs
@@ -0,0 +1,130 @@
+import {CanvasRenderer} from "./CanvasRenderer.mjs";
+import {MouseMode, TargetMode} from "../Interaction.mjs";
+
+export class CursorController extends CanvasRenderer {
+  constructor(canvas, params) {
+    super(canvas);
+    this.level = params.level;
+  }
+
+  render() {
+    if (this.level.mouseMode === MouseMode.Movement) // if cursor is movement mode
+      this.renderMoveCursor();
+    if (this.level.mouseMode === MouseMode.Target && this.level.targetMode == TargetMode.Zone)
+      this.renderZoneCursor();
+  }
+
+  renderZoneCursor() {
+    if (this.level.player && this.level.isCharacterTurn(this.level.player)) {
+      const coordinates = this.canvas.hoverTile;
+      const item = this.level.activeItem;
+
+      if (item && coordinates && coordinates.length === 2) {
+        const from = { x: coordinates[0] - item.zoneSize, y: coordinates[1] - item.zoneSize };
+        const to   = { x: coordinates[0] + item.zoneSize, y: coordinates[1] + item.zoneSize };
+
+        for (var x = from.x ; x <= to.x ; ++x) {
+          for (var y = from.y ; y <= to.y ; ++y) {
+            const tile = this.layers.ground.getTile(x, y);
+
+            if (tile)
+              this.renderImage(`qrc:/assets/ui/cursors/target-tile.png`, tile.renderPosition, this.tileSize.width, this.tileSize.height);
+          }
+        }
+      }
+    }
+  }
+
+  renderCombatMoveCursor() {
+    if (this.level.player && this.level.isCharacterTurn(this.level.player)) {
+      const coordinates = this.canvas.hoverTile;
+
+      if (coordinates && coordinates.length === 2) {
+        const path = this.level.previewPathTo(coordinates[0], coordinates[1]);
+        const maxLength = Math.min(this.level.player.actionPoints, path.length);
+
+        for (var i = 0 ; i < maxLength ; ++i) {
+          const tile = this.layers.ground.getTile(path[i].x, path[i].y);
+          const lastTile = i + 1 === maxLength;
+          const texture = lastTile ? "move-tile.png" : "move-tile-unavailable.png";
+
+          if (tile) {
+            this.renderImage(`qrc:/assets/ui/cursors/${texture}`, tile.renderPosition, this.tileSize.width, this.tileSize.height);
+            if (lastTile) {
+              this.context.font = "bold 20px sans-serif";
+              this.context.fillStyle = "#FFFFFF";
+              this.context.fillText(`${maxLength}`, tile.renderPosition.x + this.tileSize.width / 2 - 10, tile.renderPosition.y + this.tileSize.height / 2);
+              this.context.strokeStyle = "#000000";
+              this.context.strokeText(`${maxLength}`, tile.renderPosition.x + this.tileSize.width / 2 - 10, tile.renderPosition.y + this.tileSize.height / 2);
+            }
+          }
+        }
+        this.canvas.hoverTileHintVisible = !(maxLength !== path.length || path.length === 0);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  renderMoveCursor() {
+    if (!this.renderCombatMoveCursor())
+    {
+      const coordinates = this.canvas.hoverTile;
+
+      if (coordinates && coordinates.length === 2) {
+        const tile = this.layers.ground.getTile(coordinates[0], coordinates[1]);
+
+        if (tile)
+        {
+          const occupied = this.level.grid.isOccupied(coordinates[0], coordinates[1]);
+          const texture  = !occupied ? "move-tile.png" : "move-tile-unavailable.png";
+
+          this.canvas.hoverTileHintVisible = true;
+          this.renderImage(`qrc:/assets/ui/cursors/${texture}`, tile.renderPosition, this.tileSize.width, this.tileSize.height);
+          return ;
+        }
+      }
+      this.canvas.hoverTileHintVisible = false;
+    }
+  }
+
+  getHoveredCase(posX, posY) {
+    for (var x = 0 ; x < this.mapSize.width; ++x) {
+      for (var y = 0 ; y < this.mapSize.height; ++y) {
+        const pos = this.getPointFor(x, y);
+        if (posX >= pos.x && posX <= pos.x + this.tileSize.width &&
+            posY >= pos.y && posY <= pos.y + this.tileSize.height)
+        {
+          return [x, y];
+        }
+      }
+    }
+    return null;
+  }
+
+  onMouseClick(mouse, mouseX, mouseY) {
+    if (!this.level.combat || this.level.isPlayerTurn) {
+      mouseX -= this.canvas.origin.x;
+      mouseY -= this.canvas.origin.y;
+      if (this.level.mouseMode === MouseMode.Movement || (this.level.mouseMode === MouseMode.Target && this.level.targetMode === TargetMode.Zone))
+        this.onCaseClick(mouseX, mouseY);
+      else if (this.level.mouseMode === MouseMode.Target || this.level.mouseMode === MouseMode.Interaction)
+        this.onObjectClick(mouseX, mouseY);
+    }
+  }
+
+  onCaseClick(mouseX, mouseY) {
+    const coords = this.getHoveredCase(mouseX, mouseY);
+
+    console.log("onCazeClick", coords);
+    if (coords !== null)
+      this.level.tileClicked(coords[0], coords[1]);
+  }
+
+  onObjectClick(mouseX, mouseY) {
+    const object = this.level.getObjectAt(mouseX, mouseY);
+
+    if (object !== null)
+      this.level.objectClicked(object);
+  }
+}
