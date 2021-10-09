5ef9e5b5c19fa1105d5357f79312c7fda3581286
Michael <michael@unetresgrossebite.com>
2021-10-09 23:32:05 +0200

    adds missing race script for scorpions

---
+++ scripts/cmap/races/scorpion.mjs
@@ -0,0 +1,31 @@
+export const spriteSheet = "scorpion";
+
+export function onToggled(statistics, toggled) {
+  if (toggled) {
+    statistics.traits.push("scorpion");
+    statistics.traits.push("scorpion");
+    statistics.strength     -= 3;
+    statistics.perception   -= 1;
+    statistics.endurance    -= 3;
+    statistics.charisma     -= 4;
+    statistics.intelligence -= 3;
+    statistics.agility      -= 1;
+  }
+  else {
+    statistics.traits = [];
+    statistics.strength     += 3;
+    statistics.perception   += 1;
+    statistics.endurance    += 3;
+    statistics.charisma     += 4;
+    statistics.intelligence += 3;
+    statistics.agility      += 1;
+  }
+}
+
+export function modifyBaseStatistic(statistics, name, value) {
+  if (name === "maxHitPoints")
+    return value - 15;
+  if (name === "carryWeight")
+    return 5;
+  return value;
+}
