a03512082c86af1203a7ae411f7acd9646c08db4
Michael <michael@unetresgrossebite.com>
2021-10-16 21:16:53 +0200

    adds floor support to guard script component

--- scripts/pnjs/components/guard.mjs
+++ scripts/pnjs/components/guard.mjs
@@ -13,14 +13,29 @@ export class GuardComponent {
   initialize() {
     this.model.setVariable("guardX", this.model.position.x);
     this.model.setVariable("guardY", this.model.position.y);
+    this.model.setVariable("guardZ", this.model.floor);
+  }
+  
+  isAtGuardPosition() {
+    return this.model.getVariable("guardX") === this.model.position.x
+        && this.model.getVariable("guardY") === this.model.position.y
+        && this.model.getVariable("guardZ") === this.model.floor
+  }
+  
+  shouldReachGuardPosition() {
+    return this.model.actionQueue.isEmpty()
+       && !this.isAtGuardPosition()
+       && !this.model.tasks.hasTask("alarmTask");
   }
 
   task() {
-    console.log("guardingTask");
-    if (this.model.actionQueue.isEmpty() && !this.model.tasks.hasTask("alarmTask")) {
+    if (this.shouldReachGuardPosition()) {
       this.model.movementMode = "walking";
-      this.model.actionQueue.pushReachNear(
-        this.model.getVariable("guardX"), this.model.getVariable("guardY"), 1
+      this.model.actionQueue.pushReachCase(
+        this.model.getVariable("guardX"),
+        this.model.getVariable("guardY"),
+        this.model.getVariable("guardZ"),
+        0
       );
       this.model.actionQueue.start();
     }
