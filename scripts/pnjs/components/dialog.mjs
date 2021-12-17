73b726a0e70c1d182f25df381d774f35d1c9e5b5
Michael <michael@unetresgrossebite.com>
2021-12-17 02:16:24 +0100

    rewrite autotalk script helper

--- scripts/pnjs/components/dialog.mjs
+++ scripts/pnjs/components/dialog.mjs
@@ -2,15 +2,26 @@ import {MetabolismComponent} from "./metabolism.mjs";
 import {getValueFromRange} from "../../behaviour/random.mjs";
 import {skillCheck} from "../../cmap/helpers/checks.mjs";
 
+function isInAutoTalkRange(character) {
+  return character.getDistance(game.player) <= 10;
+}
+
+function canAutoTalk(character) {
+  return !level.combat && isInAutoTalkRange(character)
+}
+
 export class DialogComponent extends MetabolismComponent {
   constructor(model) {
     super(model);
   }
 
   onTalkTo() {
+    const dialog = this._delayedDialog || this.dialog;
+
     this.model.setVariable("talkedTo", 1);
-    if (this.dialog)
-      level.initializeDialog(this.model, this.dialog);
+    delete this._delayedDialog;
+    if (dialog)
+      level.initializeDialog(this.model, dialog);
     else
       this.displayRandomTextBubble();
     return true;
@@ -26,18 +37,23 @@ export class DialogComponent extends MetabolismComponent {
     }
   }
 
+  startDialog(dialog = null) {
+    game.player.fieldOfView.setCharacterDetected(this.model);
+    this.model.fieldOfView.setCharacterDetected(game.player);
+    this.model.tasks.addUniqueTask("onTalkTo", 600, 1);
+    this._delayedDialog = dialog;
+    level.cameraFocusRequired(this.model);
+  }
+
   dialogDetectionHook() {
     if (this.speakOnDetection && this.model.fieldOfView.isDetected(game.player)) {
-      if (level.combat) {
-        this.model.tasks.addTask("dialogDetectionHook", 1000, 1);
-        return false;
-      }
-      else {
-        game.player.fieldOfView.setCharacterDetected(this.model);
-        game.player.toggleSneaking(false);
-        this.onTalkTo();
+      if (canAutoTalk(this.model))
+      {
+        this.startDialog();
         return true;
       }
+      else
+        this.model.tasks.addTask("dialogDetectionHook", 1000, 1);
     }
     return false;
   }
