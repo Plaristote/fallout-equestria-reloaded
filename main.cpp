32d1d09505472b1816ad0cfb82ab7513f49d20fa
plaristote <michael@unetresgrossebite.com>
2023-04-17 12:15:22 +0200

    fixed potential issue with casting non-const Character* from JavaScript

--- main.cpp
+++ main.cpp
@@ -121,6 +121,7 @@ int main(int argc, char *argv[])
   qmlRegisterType<Credits>("Game", 1,0, "Credits");
   qmlRegisterType<CreditPerson>("Game", 1,0, "Person");
 
+  qRegisterMetaType<Character*>("Character*");
   qRegisterMetaType<Character*>("const Character*");
   qRegisterMetaType<CharacterDiplomacy*>("const CharacterDiplomacy*");
   qRegisterMetaType<DynamicObject*>("const DynamicObject*");
