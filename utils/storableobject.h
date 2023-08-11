6b27cae83462c685f4a7498aa570117a54599854
plaristote <michael@unetresgrossebite.com>
2023-08-11 12:24:32 +0200

    adds an overload to StorageObject::getVariable to set default values

--- utils/storableobject.h
+++ utils/storableobject.h
@@ -16,6 +16,7 @@ public:
 
   Q_INVOKABLE inline bool     hasVariable(const QString& name) const { return dataStore.contains(name); }
   Q_INVOKABLE inline QVariant getVariable(const QString& name) const { return dataStore[name].toVariant(); }
+  Q_INVOKABLE inline QVariant getVariable(const QString& name, QVariant defaultValue) const { return hasVariable(name) ? getVariable(name) : defaultValue; }
   Q_INVOKABLE QVariantList    getVariables(const QStringList&) const;
   Q_INVOKABLE inline void     setVariable(const QString& name, const QVariant& value) { dataStore.insert(name, QJsonValue::fromVariant(value)); }
   Q_INVOKABLE inline void     unsetVariable(const QString& name) { dataStore.remove(name); }
