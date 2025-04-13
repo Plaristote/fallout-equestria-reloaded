355274a0aa52535c0cb50a9bb3108e9f6ea4e663
plaristote <michael@unetresgrossebite.com>
2025-04-13 22:22:20 +0200

    update windeploy.bat

--- windeploy.bat
+++ windeploy.bat
@@ -1,11 +1,12 @@
 setlocal EnableDelayedExpansion
 SET "QT_DIR=C:\Qt\6.8.1\llvm-mingw_64\bin"
-SET "RELATIVE_SOURCE_DIR=src\"
-SET "windeploy_qt=%QT_DIR%\windeployqt.exe"
-SET "EXE_PATH=build\windows-release\falloutequestria.exe"
-MKDIR build\windows-release
-CD build\windows-release
+SET "EXE_NAME=falloutequestria.exe"
+SET "REL_PATH=build\windows-release"
+SET "EXE_PATH=%REL_PATH%\%EXE_PATH%"
+SET "WINDEPLOY_QT=%QT_DIR%\windeployqt.ee"
 
+MKDIR "%REL_PATH%"
 COPY "%1" "%EXE_PATH%"
+PUSHD "%REL_PATH%"
 
-%windeploy-qt%  --qmldir "..\..\%RELATIVE_SOURCE_DIR%" "%EXE_PATH%"
+%WINDEPLOY_QT%  --qmldir "..\..\src" "%EXE_NAME%"
